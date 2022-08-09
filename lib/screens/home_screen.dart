1ea50ca3eca4ba03eaefda7a0f1fc07ab10a0b8d
Isaias Medina <isamed92@gmail.com>
2022-08-08 23:25:26 -0300

    initial commit: add homescreen, settings and drawer

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:preferences_app/widgets/side_menu.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routerName = 'Home';
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      drawer: SideMenu(),
+      body: const Center(
+        child: Text('HomeScreen'),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/side_menu.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const _DrawerHeader(),
+          ListTile(
+            leading: const Icon(Icons.pages_outlined),
+            title: const Text('People'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.pages_outlined),
+            title: const Text('Settings'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _DrawerHeader extends StatelessWidget {
+  const _DrawerHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DrawerHeader(
+      child: Container(),
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/menu-img.jpg'), fit: BoxFit.cover)),
+    );
+  }
+}
